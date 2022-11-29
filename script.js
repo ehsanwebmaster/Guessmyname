491263220336d2b955128e155f6d9911b6414cb8
Ehsan Farahmand <89188159+ehsanwebmaster@users.noreply.github.com>
2022-11-29 17:13:21 +0330

    Create ifelse for check the guess Number

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 'use strict';
-const number = Math.trunc(Math.random()*20+1);
+// Create random number
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
 // Add number to Number area
-document.querySelector('.number').textContent = number;
+document.querySelector('.number').textContent = secretNumber;
+// input
+document.querySelector('.check').addEventListener('click', () => {
+  const guessValue = Number(document.querySelector('.guess').value);
+  if (!guessValue) {
+    document.querySelector('.message').textContent = '⛔ No number! ⛔';
+  } else if (guessValue === secretNumber) {
+    document.querySelector('.message').textContent = '😍 Correct Number!';
+  } else if (guessValue > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+  } else if (guessValue < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low';
+  }
+});
